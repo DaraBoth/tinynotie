a426b4c06c7a2ac442db6ce6d053a8e968b54465
daraboth <daraboth0331@gmail.com>
2024-08-10 10:57:22 +0900

    asdsad

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -663,14 +663,14 @@ const handleMessage = async function (messageObj) {
             );
         }
       } else {
-        const responseText = await callAI(messageText, chatHistory);
-        templateSaveChat({
-          Chat_ID,
-          chatHistory,
-          messageText,
-          responseText: responseText.text(),
-        });
-        return darabothSendMessage(messageObj, responseText.text());
+        // const responseText = await callAI(messageText, chatHistory);
+        // templateSaveChat({
+        //   Chat_ID,
+        //   chatHistory,
+        //   messageText,
+        //   responseText: responseText.text(),
+        // });
+        return darabothSendMessage(messageObj, "he");
       }
   }
 };
