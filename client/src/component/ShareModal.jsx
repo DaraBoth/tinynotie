e61c5b97433752a3b69a8173b26f0613f6c24ed8
daraboth <daraboth0331@gmail.com>
2024-08-22 15:16:57 +0900

    as

--- client/src/component/ShareModal.jsx
+++ client/src/component/ShareModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   Dialog,
   DialogTitle,
@@ -15,26 +15,32 @@ import {
   Select,
   OutlinedInput,
   MenuItem,
-} from '@mui/material';
-import EditIcon from '@mui/icons-material/Edit';
-import SaveIcon from '@mui/icons-material/Save';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import CustomAlert from './CustomAlert'; // Import CustomAlert component
-import currency from 'currency.js';
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import SaveIcon from "@mui/icons-material/Save";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import CustomAlert from "./CustomAlert"; // Import CustomAlert component
+import currency from "currency.js";
 
-export default function ShareModal({ open, onClose, selectedTrips, currencyType, member }) {
+export default function ShareModal({
+  open,
+  onClose,
+  selectedTrips,
+  currencyType,
+  member,
+}) {
   const [selectedTripIds, setSelectedTripIds] = useState([]);
-  const [invoiceText, setInvoiceText] = useState('');
-  const [editableText, setEditableText] = useState('');
+  const [invoiceText, setInvoiceText] = useState("");
+  const [editableText, setEditableText] = useState("");
   const [isEditing, setIsEditing] = useState(false);
-  const [selectedLanguage, setSelectedLanguage] = useState('EN'); // Default to English
+  const [selectedLanguage, setSelectedLanguage] = useState("EN"); // Default to English
   const [alertOpen, setAlertOpen] = useState(false);
-  const [alertMessage, setAlertMessage] = useState('');
-  const [alertType, setAlertType] = useState('success');
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertType, setAlertType] = useState("success");
 
   useEffect(() => {
     if (selectedTrips.length) {
-      setSelectedTripIds(selectedTrips.map(trip => trip.id));
+      setSelectedTripIds(selectedTrips.map((trip) => trip.id));
       generateInvoiceText(selectedTrips, selectedLanguage);
     }
   }, [selectedTrips, selectedLanguage]);
@@ -42,7 +48,9 @@ export default function ShareModal({ open, onClose, selectedTrips, currencyType,
   const handleTripChange = (event) => {
     const selectedIds = event.target.value;
     setSelectedTripIds(selectedIds);
-    const selected = selectedIds.map(id => selectedTrips.find(trip => trip.id === id));
+    const selected = selectedIds.map((id) =>
+      selectedTrips.find((trip) => trip.id === id)
+    );
     generateInvoiceText(selected, selectedLanguage);
   };
 
@@ -56,8 +64,8 @@ export default function ShareModal({ open, onClose, selectedTrips, currencyType,
   const generateInvoiceText = (trips, language) => {
     const translations = {
       EN: {
-        bankDetails: "[Bank name] - Toss Bank\n[XXXX-XXXX-XXXX]\n\n",
-        greeting: "Hello!\nThis is your receipt.\n",
+        bankDetails: "[Bank name]\n[XXXX-XXXX-XXXX]\n\n",
+        greeting: "Hello!\nThis is the receipt.\n\n",
         totalAmount: "Total Amount",
         participants: "Participants",
         perMember: "Per Member",
@@ -65,8 +73,8 @@ export default function ShareModal({ open, onClose, selectedTrips, currencyType,
         thankYou: "Thank you for your cooperation!",
       },
       KR: {
-        bankDetails: "[은행 이름] - 토스뱅크\n[XXXX-XXXX-XXXX]\n\n",
-        greeting: "안녕하세요!\n이것은 영수증입니다.\n",
+        bankDetails: "[은행 이름]\n[XXXX-XXXX-XXXX]\n\n",
+        greeting: "안녕하세요!\n이것은 영수증입니다.\n\n",
         totalAmount: "총 금액",
         participants: "참여 멤버",
         perMember: "1인당 금액",
@@ -74,46 +82,60 @@ export default function ShareModal({ open, onClose, selectedTrips, currencyType,
         thankYou: "협조해 주셔서 감사합니다!",
       },
       KH: {
-        bankDetails: "[ឈ្មោះធនាគារ] - ទោសបេ​ង់\n[XXXX-XXXX-XXXX]\n\n",
-        greeting: "សួស្ដី!\nនេះគឺជាវិក័យប័ត្រ។\n",
+        bankDetails: "[ឈ្មោះធនាគារ]\n[XXXX-XXXX-XXXX]\n\n",
+        greeting: "សួស្ដី!\nនេះគឺជាវិក័យប័ត្រ។\n\n",
         totalAmount: "ចំនួន​សរុប",
         participants: "អ្នក​ចូលរួម",
         perMember: "ចំណាយតាមម្នាក់",
         eachPersonPays: "ការទូទាត់តាមម្នាក់",
         thankYou: "អរគុណសម្រាប់កិច្ចសហការរបស់អ្នក!",
-      }
+      },
     };
 
     const t = translations[language];
     let totalPerMember = {};
 
-    const tripDetails = trips.map(trip => {
-      const members = JSON.parse(trip.mem_id).map(memId => {
-        const memberName = member.find(m => m.id === memId)?.mem_name;
-        return memberName;
-      });
+    const tripDetails = trips
+      .map((trip) => {
+        const members = JSON.parse(trip.mem_id).map((memId) => {
+          const memberName = member.find((m) => m.id === memId)?.mem_name;
+          return memberName;
+        });
 
-      const perMemberAmount = currency(trip.spend / members.length, { symbol: currencyType }).format();
+        const perMemberAmount = currency(trip.spend / members.length, {
+          symbol: currencyType,
+        }).format();
 
-      // Accumulate total spend per member
-      members.forEach(mem => {
-        if (!totalPerMember[mem]) {
-          totalPerMember[mem] = 0;
-        }
-        totalPerMember[mem] += trip.spend / members.length;
-      });
+        // Accumulate total spend per member
+        members.forEach((mem) => {
+          if (!totalPerMember[mem]) {
+            totalPerMember[mem] = 0;
+          }
+          totalPerMember[mem] += trip.spend / members.length;
+        });
 
-      return `${trip.trp_name}\n${t.totalAmount}: ${currency(trip.spend, { symbol: currencyType }).format()}\n${t.participants}: ${members.join(", ")}\n${t.perMember}: ${perMemberAmount}\n\n`;
-    }).join('');
+        return `${trip.trp_name}\n${t.totalAmount}: ${currency(trip.spend, {
+          symbol: currencyType,
+        }).format()}\n${t.participants}: ${members.join(", ")}\n${
+          t.perMember
+        }: ${perMemberAmount}\n\n`;
+      })
+      .join("");
 
     const totalSpend = trips.reduce((acc, trip) => acc + trip.spend, 0);
 
     // Formatting the total amount each member needs to pay
-    const perPersonDetail = Object.entries(totalPerMember).map(([name, amount]) => {
-      return `${name}: ${currency(amount, { symbol: currencyType }).format()}`;
-    }).join("\n");
+    const perPersonDetail = Object.entries(totalPerMember)
+      .map(([name, amount]) => {
+        return `${name}: ${currency(amount, {
+          symbol: currencyType,
+        }).format()}`;
+      })
+      .join("\n");
 
-    const summary = `${t.totalAmount}: ${currency(totalSpend, { symbol: currencyType }).format()}\n${t.eachPersonPays}:\n${perPersonDetail}\n\n${t.thankYou}`;
+    const summary = `${t.totalAmount}: ${currency(totalSpend, {
+      symbol: currencyType,
+    }).format()}\n${t.eachPersonPays}:\n${perPersonDetail}\n\n${t.thankYou}`;
 
     const fullText = t.bankDetails + t.greeting + tripDetails + summary;
     setInvoiceText(fullText);
@@ -121,15 +143,18 @@ export default function ShareModal({ open, onClose, selectedTrips, currencyType,
   };
 
   const handleCopyToClipboard = () => {
-    navigator.clipboard.writeText(invoiceText).then(() => {
-      setAlertMessage('Copied to clipboard!');
-      setAlertType('success');
-      setAlertOpen(true);
-    }).catch(() => {
-      setAlertMessage('Failed to copy!');
-      setAlertType('error');
-      setAlertOpen(true);
-    });
+    navigator.clipboard
+      .writeText(invoiceText)
+      .then(() => {
+        setAlertMessage("Copied to clipboard!");
+        setAlertType("success");
+        setAlertOpen(true);
+      })
+      .catch(() => {
+        setAlertMessage("Failed to copy!");
+        setAlertType("error");
+        setAlertOpen(true);
+      });
   };
 
   const handleEditToggle = () => {
@@ -145,10 +170,14 @@ export default function ShareModal({ open, onClose, selectedTrips, currencyType,
 
   return (
     <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
-      <DialogTitle sx={{ fontWeight: 'bold', fontSize: "18px" }}>Share Invoice</DialogTitle>
+      <DialogTitle sx={{ fontWeight: "bold", fontSize: "18px" }}>
+        Share Invoice
+      </DialogTitle>
       <DialogContent>
         <Box display="flex" alignItems="center" justifyContent="space-between">
-          <Typography variant="body1" sx={{ fontWeight: 'bold', mb: 1 }}>Select Trips to Share:</Typography>
+          <Typography variant="body1" sx={{ fontWeight: "bold", mb: 1 }}>
+            Select Trips to Share:
+          </Typography>
         </Box>
         <Select
           multiple
@@ -156,10 +185,16 @@ export default function ShareModal({ open, onClose, selectedTrips, currencyType,
           onChange={handleTripChange}
           input={<OutlinedInput label="Trips" />}
           renderValue={(selected) => (
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
               {selected.map((tripId) => {
-                const trip = selectedTrips.find(trip => trip.id === tripId);
-                return <Chip key={tripId} label={trip?.trp_name || tripId} variant='outlined' />;
+                const trip = selectedTrips.find((trip) => trip.id === tripId);
+                return (
+                  <Chip
+                    key={tripId}
+                    label={trip?.trp_name || tripId}
+                    variant="outlined"
+                  />
+                );
               })}
             </Box>
           )}
@@ -172,20 +207,34 @@ export default function ShareModal({ open, onClose, selectedTrips, currencyType,
           ))}
         </Select>
 
-        <Box display="flex" width={"100%"} justifyContent="center" sx={{ marginY: 2 }}>
-          <Box display="flex" width={"100%"} flexDirection={"row"} alignItems="center" justifyContent="space-between">
+        <Box
+          display="flex"
+          width={"100%"}
+          justifyContent="center"
+          sx={{ marginY: 2 }}
+        >
+          <Box
+            display="flex"
+            width={"100%"}
+            flexDirection={"row"}
+            alignItems="center"
+            justifyContent="space-between"
+          >
             <ToggleButtonGroup
               value={selectedLanguage}
               exclusive
               onChange={handleLanguageChange}
               sx={{ marginRight: 2 }}
-              size='small'
+              size="small"
             >
               <ToggleButton value="EN">EN</ToggleButton>
               <ToggleButton value="KR">KR</ToggleButton>
               <ToggleButton value="KH">KH</ToggleButton>
             </ToggleButtonGroup>
-            <IconButton onClick={handleEditToggle} color={isEditing ? "secondary" : "info"} >
+            <IconButton
+              onClick={handleEditToggle}
+              color={isEditing ? "secondary" : "info"}
+            >
               {isEditing ? <SaveIcon /> : <EditIcon />}
             </IconButton>
           </Box>
@@ -201,14 +250,19 @@ export default function ShareModal({ open, onClose, selectedTrips, currencyType,
             sx={{ mt: 2 }}
           />
         ) : (
-          <Typography variant="body2" sx={{ whiteSpace: 'pre-line', mb: 2 }}>
+          <Typography variant="body2" sx={{ whiteSpace: "pre-line", mb: 2 }}>
             {invoiceText}
           </Typography>
         )}
       </DialogContent>
 
       <DialogActions>
-        <Button onClick={handleCopyToClipboard} variant="outlined" color="secondary" startIcon={<ContentCopyIcon />}>
+        <Button
+          onClick={handleCopyToClipboard}
+          variant="outlined"
+          color="secondary"
+          startIcon={<ContentCopyIcon />}
+        >
           Copy to Clipboard
         </Button>
         <Button onClick={onClose} variant="outlined" color="secondary">
