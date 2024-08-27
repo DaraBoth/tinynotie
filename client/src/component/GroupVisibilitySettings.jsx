f9c661084f4c7fe838e67073e05e3a7342352711
daraboth <daraboth0331@gmail.com>
2024-08-27 11:31:21 +0900

    tos

--- client/src/component/GroupVisibilitySettings.jsx
+++ client/src/component/GroupVisibilitySettings.jsx
@@ -16,6 +16,7 @@ import {
   CircularProgress,
   Snackbar,
   Alert,
+  Typography,  // Add Typography for loading text
 } from "@mui/material";
 import { useGetAllUsersMutation, useUpdateGroupVisibilityMutation, useGetGroupVisibilityMutation } from "../api/api";
 
@@ -38,15 +39,20 @@ export default function GroupVisibilitySettings({ groupId, open, onClose }) {
       triggerGetVisibility({ group_id: groupId }).then((response) => {
         if (response.data?.status) {
           setVisibility(response.data.data.visibility);
-          setAllowedUsers(response.data.data.allowed_users);
-          setFetchingData(false); // Data fetched, stop loading
-        }
-      });
+          
+          // Fetch all users
+          triggerGetUsers().then((userResponse) => {
+            if (userResponse.data?.status) {
+              setUsersList(userResponse.data.data);
 
-      // Fetch all users
-      triggerGetUsers().then((response) => {
-        if (response.data?.status) {
-          setUsersList(response.data.data);
+              // Match allowed users with the full user objects
+              const allowedUserObjects = userResponse.data.data.filter(user =>
+                response.data.data.allowed_users.some(allowed => allowed.id === user.id)
+              );
+              setAllowedUsers(allowedUserObjects);
+              setFetchingData(false); // Data fetched, stop loading
+            }
+          });
         }
       });
     }
@@ -76,47 +82,56 @@ export default function GroupVisibilitySettings({ groupId, open, onClose }) {
 
   return (
     <Box>
-      <Dialog open={open && !fetchingData} onClose={onClose}>
-        <DialogTitle>Update Group Visibility</DialogTitle>
-        <DialogContent>
-          <FormControl fullWidth margin="normal">
-            <InputLabel>Visibility</InputLabel>
-            <Select
-              value={visibility}
-              onChange={handleVisibilityChange}
-              input={<OutlinedInput label="Visibility" />}
-            >
-              <MenuItem value="public">Public</MenuItem>
-              <MenuItem value="private">Private</MenuItem>
-            </Select>
-          </FormControl>
+      <Dialog open={open} onClose={onClose}>
+        {fetchingData ? (
+          <Box display="flex" justifyContent="center" alignItems="center" padding="20px">
+            <CircularProgress />
+            <Typography marginLeft="10px">Loading...</Typography> {/* Loading indicator and message */}
+          </Box>
+        ) : (
+          <>
+            <DialogTitle>Update Group Visibility</DialogTitle>
+            <DialogContent>
+              <FormControl fullWidth margin="normal">
+                <InputLabel>Visibility</InputLabel>
+                <Select
+                  value={visibility}
+                  onChange={handleVisibilityChange}
+                  input={<OutlinedInput label="Visibility" />}
+                >
+                  <MenuItem value="public">Public</MenuItem>
+                  <MenuItem value="private">Private</MenuItem>
+                </Select>
+              </FormControl>
 
-          {visibility === "private" && (
-            <FormControl fullWidth margin="normal">
-              <InputLabel>Allowed Users</InputLabel>
-              <Select
-                multiple
-                value={allowedUsers}
-                onChange={handleAllowedUsersChange}
-                input={<OutlinedInput label="Allowed Users" />}
-                renderValue={(selected) => selected.map(user => user.usernm).join(", ")}
-              >
-                {usersList.map((user) => (
-                  <MenuItem key={user.id} value={user}>
-                    <Checkbox checked={allowedUsers.indexOf(user) > -1} />
-                    <ListItemText primary={user.usernm} />
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          )}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={onClose} color="primary">Cancel</Button>
-          <Button onClick={handleSave} color="primary" disabled={loading}>
-            {loading ? <CircularProgress size="1rem" /> : "Save"}
-          </Button>
-        </DialogActions>
+              {visibility === "private" && (
+                <FormControl fullWidth margin="normal">
+                  <InputLabel>Allowed Users</InputLabel>
+                  <Select
+                    multiple
+                    value={allowedUsers}
+                    onChange={handleAllowedUsersChange}
+                    input={<OutlinedInput label="Allowed Users" />}
+                    renderValue={(selected) => selected.map(user => user.usernm).join(", ")}
+                  >
+                    {usersList.map((user) => (
+                      <MenuItem key={user.id} value={user}>
+                        <Checkbox checked={allowedUsers.indexOf(user) > -1} />
+                        <ListItemText primary={user.usernm} />
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              )}
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={onClose} color="primary">Cancel</Button>
+              <Button onClick={handleSave} color="primary" disabled={loading}>
+                {loading ? <CircularProgress size="1rem" /> : "Save"}
+              </Button>
+            </DialogActions>
+          </>
+        )}
       </Dialog>
 
       <Snackbar
