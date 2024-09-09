945a9ec84f0a6389060b996c287274b87d566cc5
daraboth <daraboth0331@gmail.com>
2024-09-09 18:05:54 +0900

    ok

--- nextjs/src/app/(root)/users/page.tsx
+++ nextjs/src/app/(root)/users/page.tsx
@@ -1,19 +1,10 @@
-import { fetchUsers } from "../../actions/userActions";
 
 export default async function UsersPage() {
-  const users = await fetchUsers();
 
   return (
     <div>
       <h1>User Information</h1>
-      {users.map((user, _) => {
-        return (
-          <div>
-            <p>Username: {user.usernm}</p>
-            <p>Email: {user.email}</p>
-          </div>
-        );
-      })}
+      
     </div>
   );
 }
