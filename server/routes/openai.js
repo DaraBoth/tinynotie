0adaf40fe24124648e031efdb44a206a8998d6ec
daraboth <daraboth0331@gmail.com>
2024-09-25 16:36:36 +0900

    again

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -231,7 +231,7 @@ router.get("/receiptText", async (req, res) => {
     const response = await result.response;
     console.log({ text });
     console.log({ res: response.text() });
-    res.status(200).json({ text: response.text() });
+    res.status(200).json({ text: JSON.parse(response.text()) });
   } catch (error) {
     console.error("error", error.message);
     res.status(500).json({ error: error.message });
