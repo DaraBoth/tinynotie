59d0fbd2fb384c6df654d2b762e7c4b731ad88d0
daraboth <daraboth0331@gmail.com>
2024-12-13 18:04:25 +0900

    jah b?

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,29 +1,30 @@
 self.addEventListener("push", (event) => {
-    let data = {};
-    if (event.data) {
-      data = event.data.json(); // Parse the push event data as JSON
-    }
-    const title = data.title || "Notification";
-    const options = {
-      body: data.body || "You have a new message!",
-      icon: "/icons/maskable_icon_x72.png", // Replace with your icon
-      badge: "/icons/maskable_icon_x72.png", // Replace with your badge
-    };
-    event.waitUntil(self.registration.showNotification(title, options));
-  });
-  
-  self.addEventListener("notificationclick", (event) => {
-    event.notification.close();
-  
-    event.waitUntil(
-      clients
-        .matchAll({ type: "window", includeUncontrolled: true })
-        .then((clientList) => {
-          if (clientList.length > 0) {
-            return clientList[0].focus();
-          }
-          return clients.openWindow("/");
-        })
-    );
-  });
-  
+  let data = {};
+  if (event.data) {
+    data = event.data.json(); // Parse the push event data as JSON
+    data = JSON.parse(data); // Parse the JSON string
+  }
+  const title = data.title || "Notification";
+  const options = {
+    body: data.body || "You have a new message!",
+    icon: "/icons/maskable_icon_x72.png", // Replace with your icon
+    badge: "/icons/maskable_icon_x72.png", // Replace with your badge
+  };
+  console.log({options});
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  event.waitUntil(
+    clients
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then((clientList) => {
+        if (clientList.length > 0) {
+          return clientList[0].focus();
+        }
+        return clients.openWindow("/");
+      })
+  );
+});
