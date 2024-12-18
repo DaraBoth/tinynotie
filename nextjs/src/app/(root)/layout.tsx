294f997d96e80528b8389e04d5b0b47bead2553b
daraboth <daraboth0331@gmail.com>
2024-12-18 18:01:05 +0900

    add side bar

--- nextjs/src/app/(root)/layout.tsx
+++ nextjs/src/app/(root)/layout.tsx
@@ -1,7 +1,18 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <main>{children}</main>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="flex h-full w-full flex-col p-4">
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
 }
