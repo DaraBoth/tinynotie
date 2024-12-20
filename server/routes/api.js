9ecf408f59c5c1ae57a12dddc5a2a1a07e52b1a8
daraboth <daraboth0331@gmail.com>
2024-12-20 16:46:21 +0900

    sa

--- server/routes/api.js
+++ server/routes/api.js
@@ -196,7 +196,7 @@ router.get("/getGroupByUserId", authenticateToken, async (req, res) => {
 router.get("/searchGroups", authenticateToken, async (req, res) => {
   const { _id: user_id } = req.user;
   const { search = "", start_date, end_date } = req.query;
-  console.log({ search = "", start_date, end_date });
+  console.log({ search, start_date, end_date });
   try {
     const sql = `
       SELECT DISTINCT g.id, g.grp_name, g.status, g.currency, g.admin_id, g.create_date,
