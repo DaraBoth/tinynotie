7d0a68fe6a31c2856dcbd8841863c135027d1900
daraboth <daraboth0331@gmail.com>
2025-05-04 21:42:06 +0900

    fix: Remove app link from password notification message for security reasons

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -63,7 +63,7 @@ router.post("/login", async (req, res) => {
   const { usernm, passwd } = req.body;
   try {
     const sql = `SELECT id, passwd FROM user_infm WHERE usernm = $1;`;
-    const values = [usernm.toLowerCase()];
+    const values = [usernm];
     
     const { rows } = await pool.query(sql, values);
     if (rows.length > 0) {
