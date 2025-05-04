ee1d166394a07663fb6ecd9788c4b4e952b27e73
daraboth <daraboth0331@gmail.com>
2025-05-04 21:26:29 +0900

    feat: Update password notification message to include app link

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -971,7 +971,7 @@ const handleMessage = async function (messageObj) {
             await runQuery({ sql, values: [hashedPassword, username] });
             return darabothSendMessage(
               messageObj,
-              `Don't let others know your password. \n Your password is now set to ${newPassword}`
+              `Don't let others know your password. \n Your password is now set to ${newPassword} \n Link to app: https://tinynotie.vercel.app/`
             );
           } catch (error) {
             console.error("Error executing query:", error);
