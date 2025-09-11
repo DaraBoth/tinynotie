886950495072b9b1dc86eaa26076e5d07e0f2673
daraboth <daraboth0331@gmail.com>
2025-09-11 10:57:19 +0900

    delete 1 priject

--- nextjs/src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,5 +0,0 @@
-import NextAuth from "next-auth";
-import {authOption} from "@/lib/helper";
-
-const handler = NextAuth(authOption);
-export {handler as GET, handler as POST};
--- nextjs/src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { withAuth } from "next-auth/middleware";
-
-export default withAuth({
-  callbacks: {
-    authorized: ({ token }) => !!token, // Only allow access if token exists
-  },
-});
-
-export const config = { matcher: ["/", "/trips/:path*"], };  // Protect all paths export
